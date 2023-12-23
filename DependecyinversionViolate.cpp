c5403da9dd96e3dbc4d3aba668e51d5b4a3ee282
Abhinav Tiwari <abhinavtiwari1729@gmail.com>
2023-12-24 02:54:38 +0530

    Dependency Inversion principle example :

---
+++ DependecyinversionViolate.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class MailSener {
+    
+    public:
+
+    void SendMail(const string & to , const string &message) {
+        cout << "sending a mail " << to << " : "  << message << endl;
+    }
+
+}; 
+
+
+
+class NotificationService{
+
+    private:
+    MailSener ms;
+
+    public:
+    NotificationService(MailSener sender) : ms(sender) {
+
+    }
+
+    void SendNotification(const string & recipient , const string & message) {
+        ms.SendMail(recipient , message);
+    }
+
+};
+
+// here NotificationService class is dependent on comcrete MailSender class which violates Denpendency Inversion Principle. 
---
+++ DependencyInversionAdhere.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+class MessageSender{
+
+    public:
+
+    virtual void SendMsg(const string &to , const string &msg) = 0 ;
+
+};
+
+
+class MailSender : public MessageSender {
+
+    public:
+    
+    void SendMsg(const string &to , const string &msg) override {
+
+        cout << "sending mail to " << to << ":" << msg << endl;
+    }
+
+};
+
+
+class NotificationService{
+
+    private:
+    MessageSender& ms;
+
+    NotificationService(MessageSender &sender) : ms(sender) {}
+
+    void SendNotification(const string & recipient , const string & msg) {
+        ms.SendMsg(recipient , msg);
+    }
+
+};
+
+
+// here we changed the dependency of NotificationService class on abstract class instead of concrete class which,
+// help in extending functionality of Notifiction service without changing other classes
